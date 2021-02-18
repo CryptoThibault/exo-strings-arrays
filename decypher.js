6df6297f4d9dc3bb3a73b65acd8e7b04077f784c
CryptoThibault <thibault.chalaoux@gmail.com>
2021-02-18 14:43:23 +0100

    chalenge

--- decypher.js
+++ decypher.js
@@ -1,13 +1,20 @@
 let code = 'RMV PFL VMVI YRU R UIVRD EVF KYRK PFL NVIV JF JLIV NRJ IVRC NYRK ZW PFL NVIV LERSCV KF NRBV WIFD KYRK UIVRD YFN NFLCU PFL BEFN KYV UZWWVIVETV SVKNVVE KYV UIVRD NFICU REU KYV IVRC NFICU'
-let t = 8
+let t = 0
+let list = []
 let index = 0
-for (i = 0; i < code.length; i++) {
-  index = code.charCodeAt(i)
-  if (index + t >= 65 && index + t <= 89) {
-    index = index++ + t
+for (j = 0; j < 25; j++) {
+  t++
+  list = []
+  for (i = 0; i < code.length; i++) {
+    index = code.charCodeAt(i)
+    if (index + t >= 65 && index + t <= 90) {
+      index = index++ + t
+    }
+    else if (index + t >= 91) {
+      index = index - 26 + t
+    }
+    list.push(String.fromCharCode(index))
   }
-  else if (index + t >= 90) {
-    index = index - 25 + t
-  }
-  console.log(String.fromCharCode(index))
+  console.log(`Essaie n°${j}`)
+  console.log(list.join('') + '\n')
 }
